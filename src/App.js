59990ad9c2153493be4f43335066164656b84618
Jatna <jatna.deilin@gmail.com>
2023-05-17 11:43:47 +0200

    Se agrega proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [characters, setCharacters] = useState([]);
+  const [houseFilter, setHouseFilter] = useState('');
 
-export default App;
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://hp-api.onrender.com/api/characters");
+      const data = await response.json();
+      setCharacters(data); // Establecer los datos en el estado
+
+    }
+
+    fetchData();
+  }, []);
+
+
+  const handleHouseFilter = (event) => {
+    setHouseFilter(event.target.value);
+  };
+
+  const filteredCharacters = characters
+    .filter(
+      (character) =>
+        character.image && character.house.toLowerCase().includes(houseFilter.toLowerCase())
+    )
+    .slice(0, 25);
+
+
+    return (
+      <div className='container'>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="🔍 Buscar"
+            value={houseFilter}
+            onChange={handleHouseFilter}
+          />
+        </div>
+        <div className="image-container">
+          {filteredCharacters.map((character) => (
+            <div className='character' key={character.id}>
+              <img src={character.image} alt={character.name} />
+              <h2>{character.name}</h2>
+              <h3>{character.house}</h3>
+              <h4>{character.patronus}</h4>
+              <h5>{character.ancestry}</h5>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+    
+  }
+  
+  export default App;
